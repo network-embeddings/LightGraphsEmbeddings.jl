6a80dff7c4fea885dcc2180648b298dfd4543b0d
Ollin Langle <ollin.langle@ciencias.unam.mx>
2019-05-26 10:19:21 -0400

    using only useful eigs

--- src/spectral/NBMembeddings.jl
+++ src/spectral/NBMembeddings.jl
@@ -1,9 +1,22 @@
-include(matrices.jl)
+include("matrices.jl")
+include("../utils.jl")
 function NBMembedding(g; matrix = ihara_matrix)
     edgeidmap, m, aristas = mapa(g)
     NBM, edgeidmap = matrix(g)
     vect = eigvecs(NBM)
-    index = 1:nv(g) |> collect
-    contracted = contraction(g,index,real(vect[:,index]),edgeidmap)
+    the_values = eigvals(NBM)
+    the_real = real.(the_values)
+    the_imag = imag.(the_values)
+    the_norm = sqrt.((the_real.^2)+(the_imag.^2))
+    both = hcat(the_imag,the_norm)
+    sorted = sortslices(both, dims=1,by=x->x[2],rev=true)
+    threshold = sorted[findall(x->x≠0,sorted[:,1]),2][1]
+    hm, index = num_com(the_values,threshold)
+    vectores = eigvecs(NBM)
+    index = index[1:length(index)]
+    matrix_embedded = real(vectores[:,index])
+    cont = contraction(g,index,matrix_embedded,edgeidmap)
+    M = fit(PCA, cont'; maxoutdim=2)
+    contracted = transform(M, cont')
     contracted
 end
